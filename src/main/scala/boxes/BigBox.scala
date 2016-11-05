7f7bd45048ad3b936cc728089dc05b05fa85a037
salim <slpg500a>
2016-11-05 17:53:16 +0000

    Add stuff

--- src/main/scala/boxes/BigBox.scala
+++ src/main/scala/boxes/BigBox.scala
@@ -3,7 +3,7 @@ package boxes
 /**
   * Created by salim on 05/11/2016.
   */
-case class BigBox(h:Int, w:Int) extends boxy {
+case class BigBox(h:Int, w:Int, littleBoxes:Map[(Int,Int),LittleBox]) extends boxy {
   def this(t:(Int,Int)) = this(t._1, t._2)
 
 }
